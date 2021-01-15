140a12284ff8ce9be7836d9beda4458fde018ec7
Ubuntu <ubuntu@ip-172-31-25-130.us-east-2.compute.internal>
2021-01-15 13:49:58 +0000

    Version_2

--- aws_network/network.tf
+++ aws_network/network.tf
@@ -70,7 +70,7 @@ resource "aws_eip" "nat" {
 resource "aws_nat_gateway" "nat" {
   count         = length(var.private_subnet_cidrs)
   allocation_id = aws_eip.nat[count.index].id
-  subnet_id     = element(aws_subnet.public_subnets[*].id, count.index)
+  subnet_id     = element(aws_subnet.private_subnets[*].id, count.index)
   tags = {
     Name = "${var.env}-nat-gw-${count.index + 1}"
   }
